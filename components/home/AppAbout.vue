f982fcf4c8d1e068e1c8f715c2e193bb8b51cbda
Andrew Westwick <awestwick@gmail.com>
2018-08-05 15:47:01 -0500

    testing

--- components/home/AppAbout.vue
+++ components/home/AppAbout.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="home-section">
     <h2>About</h2>
+    <p>Testing Netlify PR previews!!</p>
     <p>We are a web development group with members from all over the world. Some kind of mission statement and introductory paragraph, blah, blah, blah.</p>
   </section>
 </template>
