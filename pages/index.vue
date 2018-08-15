d7c000112a242147a8289c87219b6dc7d2a814ac
Caden Grey <30481164+CadenG150@users.noreply.github.com>
2018-08-15 18:48:07 -0400

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="home">
+    <test-project></test-project>
+
+  </section>
+</template>
+
+<script>
+import { TestProject } from '~/components/home';
+
+export default {
+  components: {
+    TestProject
+  }
+}
+</script>
+
+<style>
+
+</style>
