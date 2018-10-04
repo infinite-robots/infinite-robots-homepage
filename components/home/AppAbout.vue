8ef3a73aaf683ef92c33c3baf99cf23966fefed2
mfaridzia <farid19barcelona@gmail.com>
2018-10-04 21:44:04 +0700

    improve ui design

--- components/home/AppAbout.vue
+++ components/home/AppAbout.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="home-section">
-    <h2>About</h2>
-    <p>We are a web development group with members from all over the world. Some kind of mission statement and introductory paragraph, blah, blah, blah.</p>
+    <div class="content about">
+      <h2>About</h2>
+      <p>We are a web development group with members from all over the world. Some kind of mission statement and introductory paragraph, blah, blah, blah.</p>
+    </div>
   </section>
 </template>
 
--- components/home/AppProjects.vue
+++ components/home/AppProjects.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="home-section">
-    <h2>Projects</h2>
-    <p>Here's some of the things we've been working on....</p>
-    <p>Want to hire the Infinite Robots team or have us speak at an event or sponsor something or help you with something or whatever I don't know man.</p>
+    <div class="content projects">
+      <h2>Projects</h2>
+      <p>Here's some of the things we've been working on....</p>
+      <p>Want to hire the Infinite Robots team or have us speak at an event or sponsor something or help you with something or whatever I don't know man.</p>
+    </div>
   </section>
 </template>
 
